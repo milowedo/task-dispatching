0f76470e8e570d4ee97936e8fcc0952c908a1a2d
milowedo <miloszszwedo@gmail.com>
2019-02-27 00:27:51 +0100

    heavily modified the database

--- spring-api/sql-scripts/copmapny_script.sql
+++ spring-api/sql-scripts/copmapny_script.sql
@@ -3,69 +3,65 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 
-DROP SCHEMA  IF EXISTS `ticket_system`;
-CREATE SCHEMA IF NOT EXISTS `ticket_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `ticket_system` ;
+DROP SCHEMA  IF EXISTS `myjira`;
+CREATE SCHEMA IF NOT EXISTS `myjira` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `myjira` ;
 
 
--- EMPLOYEE TABLE
-drop table if exists `employee` ;
-create table if not exists `employee` (
-  `employee_id` INT(11) not null auto_increment,
-  `name` VARCHAR(45) not null,
-  `department` VARCHAR(45) not null,
-  primary key (`employee_id`)
-  )
+-- user TABLE
+drop table if exists `user`;
+drop table if exists `issue`;
+drop table if exists `issue_detail`;
+
+create table if not exists `user` (
+                                    `user_id` INT(11) not null auto_increment,
+                                    `name` VARCHAR(45) not null,
+                                    `surname` VARCHAR(45) not null,
+                                    `user_key` CHAR(6) not null,
+                                    `department` VARCHAR(45) not null,
+                                    primary key (`user_id`),
+                                    UNIQUE KEY `fullname` (`name`,`surname`),
+                                    check (user_key REGEXP '[A-Z]{6}')
+)
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8;
 
-insert into employee (employee_id, name, department) values (1, 'john locke', 'finances' ),(2, 'tyler game', 'finances'),(3, 'joe macron', 'human resources'), (4, 'rob halford', 'management');
-    
+insert into user (user_id, name, surname, user_key, department)
+values (1, 'john','locke', 'JOHLOC', 'finances' ),
+       (2, 'tyler','game', 'TYLGAM','finances'),
+       (3, 'joe','macron', 'JOEMAC','human resources'),
+       (4, 'rob','halford', 'ROBHAL','management');
 
--- TICKET TABLE
-drop table if exists `ticket` ;
-create table if not exists `ticket` (
-  `ticket_id` INT(11) not null auto_increment,
-  `status` VARCHAR(45) not null,
-  `time_created` timestamp default current_timestamp,
-  `short_desc` varchar(255),
-  primary key (`ticket_id`)
-  )
+-- ISSUE TABLE
+create table if not exists `issue` (
+                                     `issue_id` INT(11) not null auto_increment,
+                                     `issue_key` varchar(10),
+                                     `status` ENUM('To do', 'In progress', 'code review', 'done') not null,
+                                     `summary` varchar(100),
+                                     `owner` varchar(50),
+                                     `project_name` varchar(50),
+                                     primary key (`issue_id`),
+                                     check ( issue_key REGEXP '[a-zA-Z]{1,5}-[0-9]{1,7}')
+)
   ENGINE = InnoDB
-  DEFAULT CHARACTER SET = utf8;  
-  
+  DEFAULT CHARACTER SET = utf8;
 
 
-drop table if exists `ticket_detail` ;
-create table if not exists ticket_detail (
-   ticket_id int(11) not null,
-   description text,
-   category varchar(50),
-   owner varchar(50),
-   foreign key(ticket_id) references ticket (ticket_id)
+create table if not exists `issue_detail` (
+                                            `issue_id` int(11) not null,
+                                            `description` text,
+                                            `priority` TINYINT not null,
+                                            `assigned` CHAR(6),
+                                            `created` timestamp default current_timestamp,
+                                            `updated` timestamp default current_timestamp,
+                                            foreign key(issue_id) references issue (issue_id),
+                                            check ( priority between 1 and 9)
 )
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4;
 
-insert into ticket_detail values
-  (1,
-  'somebody once told me',
-  'money laundering',
-  'john berlusco');
-
-
--- EMPLOYEE`S TICKETS
-drop table if exists `employee_tickets`;
-create table if not exists `employee_tickets` (
-	`employee_id` INT(11),
-  `ticket_id` INT(11),
-  foreign key (`employee_id`) references employee (`employee_id`),
-  foreign key (`ticket_id`) references ticket (`ticket_id`)
-    )
-  ENGINE = InnoDB
-  DEFAULT CHARACTER SET = utf8;  
-    
-insert into employee_tickets values(1,1);
+insert into issue_detail (issue_id, description, priority, assigned)
+values (1,'somebody once told me there was a problem',5,'JOHLOC');
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
