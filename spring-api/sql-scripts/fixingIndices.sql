37df30db73e3d0b1066be8ac3284c075e127268c
milowedo <miloszszwedo@gmail.com>
2019-02-27 23:02:43 +0100

    old db actions

--- spring-api/sql-scripts/fixingIndices.sql
+++ spring-api/sql-scripts/fixingIndices.sql
@@ -1,18 +1,18 @@
 create temporary table ticket_system.indices
 select ticket_id as id,
        (select count(ticket_id)
-        from ticket_system.ticket
+        from ticket_system.issue
         where ticket_id < id)+1 as actual_id
-from ticket_system.ticket
+from ticket_system.issue
 
 
 SET SQL_SAFE_UPDATES = 0;
 
-update ticket_system.ticket set ticket_id =
+update ticket_system.issue set ticket_id =
 (select actual_id from ticket_system.indices where id = ticket_id)
 where ticket_id is not null
 
 
-select * from ticket_system.ticket
+select * from ticket_system.issue
 
 drop temporary table ticket_system.indices
