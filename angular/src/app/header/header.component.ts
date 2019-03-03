810847730f735752ca006b3547f3976e4e8e6700
milowedo <miloszszwedo@gmail.com>
2019-03-04 00:39:11 +0100

    bootstrap upgrade fix

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent{
+export class HeaderComponent {
+  isCollapsed = true;
+
+
+  constructor(              private router: Router) {
+  }
+
+  newIssue() {
+    this.router.navigate(['/issue/new'] );
+  }
+
+  issues() {
+    this.router.navigate(['/issues/'] );
+  }
 }
