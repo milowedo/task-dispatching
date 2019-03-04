1050e27135c64181f9ffdc291df0ccda75033ac7
milowedo <miloszszwedo@gmail.com>
2019-03-04 16:55:16 +0100

    added detail acquisition, single object getter

--- angular/src/app/services/issue.service.ts
+++ angular/src/app/services/issue.service.ts
@@ -1,35 +1,60 @@
 import {Issue} from '../entities/issue.model';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {api_address} from '../globals/globals';
+import {api_address, issueDetail_endpoint} from '../globals/globals';
 import {Subject} from 'rxjs';
+import {IssueDetail} from '../entities/issueDetail.model';
 
 @Injectable()
 export class IssueService {
 
   private issues: Issue[] = [];
-  issueChanges = new Subject<Issue[]>();
+  issueChanged = new Subject<Issue[]>();
+
+  private detail: IssueDetail = null;
+  detailChanged = new Subject<IssueDetail>();
+
   private httpOptions = {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
   constructor(private httpClient: HttpClient) {}
 
-  public fetchAll() {
+  public fetchAllIssues() {
     this
       .httpClient
       .get<Issue[]>((api_address+'issue/all'), this.httpOptions)
       .subscribe(
-       res => this.setAll(res)
+       res => this.setIssues(res)
       );
   }
-
-  public setAll(values: Issue[]){
+  private setIssues(values: Issue[]){
     this.issues = values;
-    this.issueChanges.next(this.issues.slice());
+    this.issueChanged.next(this.issues.slice());
+  }
+  public getIssues(){
+    return this.issues;
+  }
+  public getSingleIssue(id: number) {
+    return this.issues[id - 1];
   }
 
-  public getAll(){
-    return this.issues;
+  public fetchDetail(id: number){
+    let address = api_address+issueDetail_endpoint+id.toString();
+    console.log(address);
+    this
+      .httpClient
+      .get<IssueDetail>(address, this.httpOptions)
+      .subscribe(
+        res => this.setDetail(res)
+      );
+  }
+  private setDetail(res: IssueDetail) {
+    this.detail = res;
+    this.detailChanged.next(this.detail);
+
+  }
+  public getDetail(){
+    return this.detail;
   }
 }
