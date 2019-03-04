a0c175fd96ff7f7b5a43bd2d3fd10ae5e0c9fde6
milowedo <miloszszwedo@gmail.com>
2019-03-04 16:56:17 +0100

    fetches issue data

--- angular/src/app/issue/full-issue/full-issue.component.ts
+++ angular/src/app/issue/full-issue/full-issue.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {IssueService} from '../../services/issue.service';
+import {Subscription} from 'rxjs';
+import {IssueDetail} from '../../entities/issueDetail.model';
+import {Issue} from '../../entities/issue.model';
 
 @Component({
   selector: 'app-full-issue',
   templateUrl: './full-issue.component.html',
 })
-export class FullIssueComponent implements OnInit {
+export class FullIssueComponent implements OnInit, OnDestroy {
   id : number;
-  constructor( private route: ActivatedRoute) { }
+  private subscription: Subscription;
 
-  ngOnInit() {
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id = +params['id'];
-        }
-      )
+  issue: Issue = null;
+  details: IssueDetail = null;
+
+  constructor( private route: ActivatedRoute,
+               private issueService: IssueService) {
+  }
+
+  ngOnInit() :void {
+    this.route.params.subscribe((params: Params) => this.id = +params['id']);
+    this.issueService.fetchDetail(this.id);
+    this.details = this.issueService.getDetail();
+    this.subscription = this.issueService.detailChanged.subscribe(
+      (newDetail : IssueDetail) => this.details = newDetail
+    );
+    this.issue = this.issueService.getSingleIssue(this.id);
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
