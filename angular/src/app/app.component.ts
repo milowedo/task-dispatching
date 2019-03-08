ed0f36341f61fecd37144499ec3ba7f498b8a8de
milowedo <miloszszwedo@gmail.com>
2019-03-08 02:04:34 +0100

    show header only if logged in

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+  logged: boolean = false;
+  constructor(private userService : UserService){}
+
+  ngOnInit(): void {
+    this.userService.isLoggedIn()
+      .then(
+        (auth : boolean) => {
+          this.logged = auth;
+        }
+      )
+  }
+
+}
